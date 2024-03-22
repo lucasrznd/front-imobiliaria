741d1f14ff7656151e8f61e22253c79cadaa898b
Lucas Rezende <lucasffrezende@gmail.com>
2024-03-22 16:10:54 -0300

    :sparkles: feat: Rodape

---
+++ src/components/Rodape.jsx
@@ -0,0 +1,16 @@
+export default function Rodape() {
+    return (
+        <div style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent:  "center",
+            backgroundColor: "#406d48",
+            color: "white",
+            height: "60px",
+            borderRadius: "5px",
+            marginTop: "10px"
+        }}>
+            <span>SGI - Todos os Direitos Reservados.&copy;</span>
+        </div>
+    )
+}
