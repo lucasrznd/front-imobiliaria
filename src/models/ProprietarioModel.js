1cd7579d4dedb7de8d7c026c0ee9526177f6200b
Lucas Rezende <lucasffrezende@gmail.com>
2024-03-26 14:57:57 -0300

    :boom: fix: Adicionado Construtor

--- src/models/ProprietarioModel.js
+++ src/models/ProprietarioModel.js
@@ -1,6 +1,10 @@
 export class ProprietarioModel {
-    id
-    nome
-    telefone
-    ativo
+    
+    constructor(id, nome, telefone, ativo) {
+        this.id = id
+        this.nome = nome
+        this.telefone = telefone
+        this.ativo = ativo
+    }
+
 }
