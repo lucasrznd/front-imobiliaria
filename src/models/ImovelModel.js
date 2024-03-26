5770a54892b751ca27ed35b21826f51b2e03c878
Lucas Rezende <lucasffrezende@gmail.com>
2024-03-26 16:09:05 -0300

    :sparkles: feat: ImovelModel

---
+++ src/models/ImovelModel.js
@@ -0,0 +1,13 @@
+export class ImovelModel {
+
+    constructor(id, titulo, descricao, proprietario, endereco, valorTotal, disponibilidadeTempo) {
+        this.id = id
+        this.titulo = titulo
+        this.descricao = descricao
+        this.proprietario = proprietario
+        this.endereco = endereco
+        this.valorTotal = valorTotal
+        this.disponibilidadeTempo = disponibilidadeTempo
+    }
+
+}
