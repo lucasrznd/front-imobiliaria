11704ed8e9dc2358ed3bfec2565efdb9c87df175
Lucas Rezende <lucasffrezende@gmail.com>
2024-03-27 14:52:01 -0300

    :sparkles: feat: Página de Imóveis

--- src/pages/Imovel.jsx
+++ src/pages/Imovel.jsx
@@ -11,16 +11,28 @@ import Rodape from "../components/Rodape";
 import { Dialog } from "primereact/dialog";
 import { InputText } from "primereact/inputtext";
 import { InputTextarea } from "primereact/inputtextarea";
+import { AutoComplete } from "primereact/autocomplete";
+import { InputNumber } from "primereact/inputnumber";
+import { Calendar } from "primereact/calendar";
+import { EnderecoModel } from "../models/EnderecoModel";
 
 export default function CadastroImovel() {
     const [imovel, setImovel] = useState(new ImovelModel());
     const [imoveis, setImoveis] = useState([
-        { id: 1, titulo: 'Casa', descricao: 'Casa grande, dois andares, dois banheiros, 3 quartos.', }
+        {
+            id: 1, titulo: 'Casa', descricao: 'Casa grande, dois andares, dois banheiros, 3 quartos.', proprietario: 'Guilherme', valorTotal: 2000, disponibilidadeTempo: 6, ativo: 'Sim'
+        }
     ]);
+    const [endereco, setEndereco] = useState(new EnderecoModel());
     const [detalhesVisible, setDetalhesVisible] = useState(false);
     const [buscarVisible, setBuscarVisible] = useState(false);
     const [deleteImovelDialog, setDeleteImovelDialog] = useState(false);
+    const [items, setItems] = useState([]);
     const toast = useRef();
+
+    const buscaAutocomplete = (event) => {
+        setItems([...Array(10).keys()].map(item => event.query + '-' + item));
+    }
 
     const conteudoInicial = (
         <React.Fragment>
@@ -32,6 +44,7 @@ export default function CadastroImovel() {
 
     function novoImovelAction() {
         setImovel(new ImovelModel());
+        setEndereco(new EnderecoModel());
         setDetalhesVisible(true);
     }
 
@@ -39,6 +52,16 @@ export default function CadastroImovel() {
         setImovel(new ImovelModel());
         setBuscarVisible(true);
     }
+
+    const acoesDataTable = (rowData) => {
+        return (
+            <React.Fragment>
+                <Button icon="pi pi-pencil" rounded className="mr-2" onClick={() => detalhesImovel(rowData)} />
+                <span style={{ margin: "15px" }} className="pi pi-ellipsis-v"></span>
+                <Button icon="pi pi-trash" rounded severity="danger" onClick={() => confirmDeleteImovel(rowData)} />
+            </React.Fragment>
+        );
+    };
 
     const detalhesImovel = (imovel) => {
         setImovel({ ...imovel });
@@ -54,7 +77,7 @@ export default function CadastroImovel() {
     }
 
     const deletarImovel = () => {
-        const novaLista = imoveis.filter(i => i.nome !== imovel.nome);
+        const novaLista = imoveis.filter(i => i.titulo !== imovel.titulo);
         setImoveis(novaLista);
         setDeleteImovelDialog(false);
         msgAviso('Imóvel removido com sucesso.');
@@ -78,6 +101,7 @@ export default function CadastroImovel() {
 
     const salvarImovelAction = () => {
         setDetalhesVisible(false);
+        console.log(imovel);
         imoveis.push(imovel);
         msgSucesso('Imóvel cadastrado com sucesso.');
     }
@@ -113,6 +137,26 @@ export default function CadastroImovel() {
         </div>
     );
 
+    const changeEnderecoRua = (ev) => {
+        setEndereco({ ...endereco, rua: ev.target.value });
+        setImovel({ ...imovel, endereco: endereco });
+    }
+
+    const changeEnderecoBairro = (ev) => {
+        setEndereco({ ...endereco, bairro: ev.target.value });
+        setImovel({ ...imovel, endereco: endereco });
+    }
+
+    const changeEnderecoNumero = (ev) => {
+        setEndereco({ ...endereco, numero: ev.target.value });
+        setImovel({ ...imovel, endereco: endereco });
+    }
+
+    const changeEnderecoCidade = (ev) => {
+        setEndereco({ ...endereco, cidade: ev.target.value });
+        setImovel({ ...imovel, endereco: endereco });
+    }
+
     return (
         <div>
             <MenuApp />
@@ -131,9 +175,11 @@ export default function CadastroImovel() {
                             <Column field="titulo" header="Título" align="center" alignHeader="center"></Column>
                             <Column field="descricao" header="Descrição" align="center" alignHeader="center" />
                             <Column field="proprietario" header="Proprietário" align="center" alignHeader="center" />
-                            <Column field="endereco" header="Endereço" align="center" alignHeader="center" />
+                            <Column field="endereco.rua" header="Endereço" align="center" alignHeader="center" />
                             <Column field="valorTotal" header="Valor Total" align="center" alignHeader="center" />
-                            <Column field="disponibilidadeTempo" header="Disponibilidade Tempo" align="center" alignHeader="center" />
+                            <Column field="disponibilidadeTempo" header="Disponibilidade (meses)" align="center" alignHeader="center" />
+                            <Column field="ativo" header="Ativo" align="center" alignHeader="center" />
+                            <Column body={acoesDataTable} exportable={false} style={{ minWidth: '12rem' }} align="center" header="Ações" alignHeader="center" />
                         </DataTable>
                     </div>
                 </Panel>
@@ -142,23 +188,70 @@ export default function CadastroImovel() {
                     footer={rodapeModal} draggable={false}>
                     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                         <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start', marginBottom: '1rem' }}>
-                            <div style={{ display: 'flex', flexDirection: 'column', marginRight: '1rem' }}>
+                            <div style={{ display: 'flex', flexDirection: 'column', marginRight: '1rem', flex: 1 }}>
                                 <label htmlFor='nome' style={{ marginBottom: '0.5rem' }}>Nome:</label>
-                                <InputText id="nome" value={imovel.titulo} onChange={(e) => setImovel({ ...imovel, titulo: e.target.value })} style={{ width: '300px' }} />
+                                <InputText id="nome" value={imovel.titulo} onChange={(e) => setImovel({ ...imovel, titulo: e.target.value })} style={{ width: '100%' }} />
                             </div>
-
-                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                            <div style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
                                 <label htmlFor='proprietario' style={{ marginBottom: '0.5rem' }}>Proprietário:</label>
-                                <InputText id="proprietario" value={imovel.proprietario} onChange={(e) => setImovel({ ...imovel, proprietario: e.target.value })} style={{ width: '300px' }} />
+                                <AutoComplete value={imovel.proprietario} suggestions={items} completeMethod={buscaAutocomplete} onChange={(e) => setImovel({ ...imovel, proprietario: e.target.value })} style={{ width: '100%' }} />
                             </div>
                         </div>
 
-                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginBottom: '1rem' }}>
+                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginBottom: '1rem', width: '100%', maxWidth: '28vw' }}>
                             <label htmlFor='descricao' style={{ marginBottom: '0.5rem' }}>Descrição:</label>
-                            <InputTextarea id="descricao" value={imovel.descricao} onChange={(e) => setImovel({ ...imovel, descricao: e.target.value })} style={{ width: '615px' }} />
+                            <InputTextarea id="descricao" value={imovel.descricao} onChange={(e) => setImovel({ ...imovel, descricao: e.target.value })} style={{ width: '100%' }} />
+                        </div>
+
+                        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start' }}>
+                            <div style={{ display: 'flex', flexDirection: 'column', marginRight: '1rem', flex: 2 }}>
+                                <label htmlFor='disponibilidadeTempo' style={{ marginBottom: '0.5rem' }}>Disponibilidade (meses):</label>
+                                <InputNumber inputId="disponibilidadeTempo" value={imovel.disponibilidadeTempo} onValueChange={(e) => setImovel({ ...imovel, disponibilidadeTempo: e.target.value })} mode="decimal" showButtons min={1} max={12} size={15} />
+                            </div>
+
+                            <div style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
+                                <label htmlFor='valorTotal' style={{ marginBottom: '0.5rem' }}>Valor Total:</label>
+                                <InputNumber id="valorTotal" value={imovel.valorTotal} onValueChange={(e) => setImovel({ ...imovel, valorTotal: e.target.value })} mode="currency" currency="BRL" locale="pt-BR" style={{ width: "100%" }} placeholder="R$ 2.000,00" />
+                            </div>
+                        </div>
+
+                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', width: '100%', maxWidth: '28vw' }}>
+                            <h2 style={{ color: "#069669" }}>Endereço</h2>
+                        </div>
+
+                        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start', marginBottom: '1rem', width: '66%' }}>
+                            <div style={{ display: 'flex', flexDirection: 'column', marginRight: '1rem', flex: 1 }}>
+                                <label htmlFor='rua' style={{ marginBottom: '0.5rem' }}>Rua:</label>
+                                <InputText id="rua" value={endereco.rua} onChange={changeEnderecoRua} style={{ width: '100%' }} />
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
+                                <label htmlFor='bairro' style={{ marginBottom: '0.5rem' }}>Bairro:</label>
+                                <InputText id="bairro" value={endereco.bairro} onChange={changeEnderecoBairro} style={{ width: '100%' }} />
+                            </div>
+                        </div>
+
+                        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start', width: '66%' }}>
+                            <div style={{ display: 'flex', flexDirection: 'column', marginRight: '1rem', flex: 1 }}>
+                                <label htmlFor='numero' style={{ marginBottom: '0.5rem' }}>Número:</label>
+                                <InputText id="numero" value={endereco.numero} onChange={changeEnderecoNumero} style={{ width: '100%' }} />
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
+                                <label htmlFor='cidade' style={{ marginBottom: '0.5rem' }}>Cidade:</label>
+                                <InputText id="cidade" value={endereco.cidade} onChange={changeEnderecoCidade} style={{ width: '100%' }} />
+                            </div>
                         </div>
                     </div>
+                </Dialog>
 
+                <Dialog visible={deleteImovelDialog} style={{ width: '32rem' }} breakpoints={{ '960px': '75vw', '641px': '90vw' }} header="Confirmação" modal footer={deleteImovelDialogFooter} onHide={hideDeleteImovelDialog}>
+                    <div className="confirmation-content" style={{ display: "flex", alignItems: "center" }}>
+                        <i className="pi pi-exclamation-triangle mr-3" style={{ fontSize: '2rem' }} />
+                        {imovel && (
+                            <span>
+                                Deseja realmente excluir o registro?
+                            </span>
+                        )}
+                    </div>
                 </Dialog>
 
                 <Rodape />
