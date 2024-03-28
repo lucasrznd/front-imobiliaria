0475bd5d70373b2d6534b1c3be443c3e39ca61c1
Lucas Rezende <lucasffrezende@gmail.com>
2024-03-28 15:39:24 -0300

    :recycle: refactor: Identação

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,15 +5,15 @@ import { Password } from 'primereact/password';
 import { Button } from 'primereact/button';
 import { useState } from "react";
 import logo from './../assets/images/logo.png';
-import retangulo from './../assets/images/Rectangle 1.png'
+import retangulo from './../assets/images/retangulo1.png'
 import homem from './../assets/images/homem.png'
 import luminaria from './../assets/images/luminaria.png'
 
-export default function Login(){
+export default function Login() {
     const [value, setValue] = useState('');
 
-    return(
-        <Fragment>
+    return (
+        <>
             <div className="fundo">
                 <div className="blocoum">
                     <div>
@@ -26,30 +26,27 @@ export default function Login(){
                     <div>
                         <img className="imagem" src={logo} alt="" />
                         <div className="senha" >Usuário: </div>
-                    <div className="p-inputgroup flex-1">
-                        <span className="p-inputgroup-addon">
-                            <i className="pi pi-user"></i>
-                        </span>
-                        <InputText placeholder="Username" />
-                    </div>
-                    <div className="senha">Senha: </div>
-                    <div>
-                    <div className="p-inputgroup flex-1">
-                        <span className="p-inputgroup-addon">
-                            <i className="pi pi-lock"></i>
-                        </span>
-                        <Password  placeholder="Senha" value={value} onChange={(e) => setValue(e.target.value)} feedback={false} tabIndex={1} />
-                         
-                    </div>
-                        
-                       
-                    </div>
-                    <div>
-                        <Button label="Entrar" />
-                    </div>
+                        <div className="p-inputgroup flex-1">
+                            <span className="p-inputgroup-addon">
+                                <i className="pi pi-user"></i>
+                            </span>
+                            <InputText placeholder="Username" />
+                        </div>
+                        <div className="senha">Senha: </div>
+                        <div>
+                            <div className="p-inputgroup flex-1">
+                                <span className="p-inputgroup-addon">
+                                    <i className="pi pi-lock"></i>
+                                </span>
+                                <Password placeholder="Senha" value={value} onChange={(e) => setValue(e.target.value)} feedback={false} tabIndex={1} />
+                            </div>
+                        </div>
+                        <div>
+                            <Button label="Entrar" />
+                        </div>
                     </div>
                 </div>
             </div>
-        </Fragment>
+        </>
     )
 }
