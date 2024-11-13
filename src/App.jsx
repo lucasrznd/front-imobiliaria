97d8e2a34c6aa15c6b76a8be310f58f103a6a948
Lucas Rezende <lucasffrezende@gmail.com>
2024-11-13 17:53:19 -0300

    fix: imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import './App.css';
 import 'primereact/resources/themes/lara-light-green/theme.css';
 import 'primeicons/primeicons.css';
-import '/node_modules/primeflex/primeflex.css';
+import 'primeflex/primeflex.css';
 import AppRoutes from './routes/Routes';
 
 function App() {
